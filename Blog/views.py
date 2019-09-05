83445a849a099245bc2b97aace10ee5408207446
BMaheshwari <bhupali.maheshwari@gmail.com>
2019-09-05 11:12:32 +0530

    class based views implemented

--- Blog/views.py
+++ Blog/views.py
@@ -9,7 +9,9 @@ from django.views.generic.base import View, TemplateView
 from django.contrib.auth.decorators import login_required
 
 from .models import Post, Author
-from .forms import AddBlog, SignupForm, SearchForm
+from .forms import AddBlog, SearchForm, SignupForm
+
+
 # Create your views here.
 # def index(request):
 #     # if request.POST:
@@ -149,13 +151,15 @@ class add_blog(FormView):
     template_name = 'Blog/ShowForm.html'
     success_url = reverse_lazy('displayBlog')
 
-    @login_required
     def form_valid(self, form):
+        # print("inside form_valid")
         try:
-            user1 = Author.objects.get(user=self.request.user)
+            # print("inside try")
+            user1 = Author.objects.get(user = self.request.user)
             # print(user1.first_name)
         except:
-            user1 = Author.objects.create(user=self.request.user)
+            # print("inside except")
+            user1 = Author.objects.create(user = self.request.user)
             # user1.save()
             # print(user1.first_name)
 
@@ -174,23 +178,25 @@ class add_blog(FormView):
 class signUp(CreateView):
     form_class = SignupForm
     model = User
+    # fields = ['username', 'first_name', 'last_name', 'email', 'password']
     success_url = reverse_lazy('displayBlog')
-    template_name = 'registration/signup.html'
+    #template_name = 'registration/signup.html'
 
-    def form_valid(self, form):
-        return super(signUp, self).form_valid(form)
+    # def form_valid(self, form):     #Model form views provide a form_valid() implementation that saves the model automatically. You can override this if you have any special requirements;
+    #     return super(signUp, self).form_valid(form)
 
 
 
 class UpdatesignUp(UpdateView):
     form_class = SignupForm     #form_class and fields cannot be together
     model = User
+    #fields = ['username', 'first_name', 'last_name', 'email', 'password']
     success_url = reverse_lazy('displayBlog')
     template_name = 'registration/signup.html'
     #fields = ('__all__')
 
-    def form_valid(self, form):
-        return super(UpdatesignUp, self).form_valid(form)
+    # def form_valid(self, form):
+    #     return super(UpdatesignUp, self).form_valid(form)
 
 
 
@@ -231,7 +237,7 @@ def search_blog(request):
 
 # class BlogDetailView(View):
 #     def get(self, request,**kwargs):
-#         blog = get_object_or_404(Post, id=kwargs['pk'])
+#         blog = get_object_or_404(Post, id=kwargs['pk'])   #In a class-based view, all of the elements from the URL are placed into self.args (if they're non-named groups) or self.kwargs (for named groups). So, for your view, you can use self.kwargs['pk']
 #         context = {'blog' : blog }
 #         return render(request, 'Blog/blog_detail.html', context)
 
